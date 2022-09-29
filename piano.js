398e5daabe3f3a0a38b8e5e420a01223e34c3917
coachemmy <coachemmyb@gmail.com>
2022-09-30 00:47:55 +0100

    piano

--- piano.js
+++ piano.js
@@ -6,7 +6,7 @@ function playSound(newUrl){
 }
 
 pianoKeys.forEach((pianoKey, i) => {
-    const reMap = ['09', '11', '14', ]
+    const reMap = [02, 04, 06, 09, 11, 14, 16, 18, 21, 23, 01, 03, 05, 07, 08, 10, 12, 13, 15, 17, 19, 20, 22, 24 ]
     const number = i < 9 ? '0' + (i + 1) : (i + 1)
     const newUrl = '24-piano-keys/key' + number + '.mp3'
     console.log(newUrl)
